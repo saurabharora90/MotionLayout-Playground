86add85cf7e082d6eacdfc6057b1088d8577b5c1
Saurabh Arora <saurabharora90@gmail.com>
2018-09-29 19:12:47 +0800

    Push project

---
+++ app/src/main/java/com/saurabharora/motionlayout/playground/ConstraintStateRobinhood.kt
@@ -0,0 +1,12 @@
+package com.saurabharora.motionlayout.playground
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintStateRobinhood : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_state__robinhood)
+    }
+}
