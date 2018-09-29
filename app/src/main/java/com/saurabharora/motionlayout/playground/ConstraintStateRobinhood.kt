bd58bf3a182b43116323dbacca779206a7e6214c
Saurabh Arora <saurabharora90@gmail.com>
2018-09-29 19:38:44 +0800

    build robinhood example using constraint states

--- app/src/main/java/com/saurabharora/motionlayout/playground/ConstraintStateRobinhood.kt
+++ app/src/main/java/com/saurabharora/motionlayout/playground/ConstraintStateRobinhood.kt
@@ -1,12 +1,34 @@
 package com.saurabharora.motionlayout.playground
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintsChangedListener
+import androidx.transition.TransitionManager
+import kotlinx.android.synthetic.main.activity_robinhood.*
 
+/**
+ * This Layout animation shown here is found in the Robinhood app and is talked about in the following post:
+ * https://robinhood.engineering/beautiful-animations-using-android-constraintlayout-eee5b72ecae3
+ *
+ * The post only shows the animation but doesn't show the code on how to build it.
+ *
+ * I have tried to make it as close as possible
+ */
 class ConstraintStateRobinhood : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_constraint_state__robinhood)
+        setContentView(R.layout.activity_robinhood)
+
+        root.loadLayoutDescription(R.xml.robinhood_constraint_state)
+
+        fab.setOnClickListener { root.setState(R.id.end, 0, 0) }
+        ivSwipeUp.setOnClickListener { root.setState(R.id.start, 0, 0) }
+
+        root.setOnConstraintsChanged(object : ConstraintsChangedListener() {
+            override fun preLayoutChange(stateId: Int, constraintId: Int) {
+                TransitionManager.beginDelayedTransition(root)
+            }
+        })
     }
 }
