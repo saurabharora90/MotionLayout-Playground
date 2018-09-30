07d16564cf8a6615d0a01c34c5d77d97834eff50
Saurabh Arora <saurabharora90@gmail.com>
2018-09-30 12:44:18 +0800

    main activity as a entry to all other examples

---
+++ app/src/main/java/com/saurabharora/motionlayout/playground/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.saurabharora.motionlayout.playground
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.tvConstraintState -> {
+                startActivity(Intent(this, ConstraintStateRobinhood::class.java))
+            }
+            R.id.tvSimpleMotion -> {
+                startActivity(Intent(this, ConstraintStateRobinhood::class.java))
+            }
+            R.id.tvTwoViews -> {
+                startActivity(Intent(this, ConstraintStateRobinhood::class.java))
+            }
+            R.id.tvttributes -> {
+                startActivity(Intent(this, ConstraintStateRobinhood::class.java))
+            }
+            R.id.tvKeyFrame -> {
+                startActivity(Intent(this, ConstraintStateRobinhood::class.java))
+            }
+        }
+    }
+}
