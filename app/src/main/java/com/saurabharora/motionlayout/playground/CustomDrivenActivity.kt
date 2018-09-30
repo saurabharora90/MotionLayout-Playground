0f75c9fe43bf7b8f02988cbafa968444d79b3053
Saurabh Arora <saurabharora90@gmail.com>
2018-10-01 00:44:15 +0800

    recycler view to scroll the app bar

--- app/src/main/java/com/saurabharora/motionlayout/playground/CustomDrivenActivity.kt
+++ app/src/main/java/com/saurabharora/motionlayout/playground/CustomDrivenActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.saurabharora.motionlayout.playground.adapter.AnimalAdapter
 import kotlinx.android.synthetic.main.activity_custom.*
 
 class CustomDrivenActivity : AppCompatActivity() {
@@ -15,5 +17,17 @@ class CustomDrivenActivity : AppCompatActivity() {
 
         val size = resources.getDimensionPixelSize(R.dimen.profile_image_size)
         ivProfile.setImageBitmap(ContextCompat.getDrawable(this, R.drawable.droid)?.toBitmap(width = size, height = size))
+
+        val animalNames = mutableListOf<String>()
+        animalNames.add("Horse")
+        animalNames.add("Cow")
+        animalNames.add("Camel")
+        animalNames.add("Sheep")
+        animalNames.add("Goat")
+
+        rv.apply {
+            layoutManager = LinearLayoutManager(this@CustomDrivenActivity)
+            adapter = AnimalAdapter(this@CustomDrivenActivity, animalNames)
+        }
     }
 }
---
+++ app/src/main/java/com/saurabharora/motionlayout/playground/adapter/AnimalAdapter.kt
@@ -0,0 +1,37 @@
+package com.saurabharora.motionlayout.playground.adapter
+
+import android.content.Context
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import android.view.View
+import com.saurabharora.motionlayout.playground.R
+
+
+class AnimalAdapter(context: Context, private val mData: List<String>) : RecyclerView.Adapter<AnimalAdapter.ViewHolder>() {
+    private val mInflater: LayoutInflater = LayoutInflater.from(context)
+
+    // inflates the row layout from xml when needed
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = mInflater.inflate(R.layout.recyclerview_row, parent, false)
+        return ViewHolder(view)
+    }
+
+    // binds the data to the TextView in each row
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val animal = mData[position]
+        holder.myTextView.text = animal
+    }
+
+    // total number of rows
+    override fun getItemCount(): Int {
+        return mData.size
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    inner class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        internal var myTextView: TextView = itemView.findViewById(R.id.tvAnimalName)
+    }
+}
