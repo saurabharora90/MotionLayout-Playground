5fae7e71beb7037499976a634f3874153e8feb0b
Saurabh Arora <saurabharora90@gmail.com>
2020-12-30 17:29:10 +0800

    Add a mock of the Google Pay card selector

---
+++ app/src/main/java/com/saurabharora/motionlayout/playground/GooglePayCardSelectorActivity.kt
@@ -0,0 +1,11 @@
+package com.saurabharora.motionlayout.playground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GooglePayCardSelectorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_google_pay_card_selector)
+    }
+}
